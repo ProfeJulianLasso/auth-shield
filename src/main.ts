a31896425b7d5c10abeff1699266cafd6747af46
Julian Lasso <julian.lasso@sofka.com.co>
2023-06-05 16:12:37 -0500

    Estructura inicial del microservicio

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+// Libraries
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+// Main module
+import { AuthShieldModule } from './infrastructure/auth-shield.module';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AuthShieldModule);
   await app.listen(3000);
+  console.log(`🚀 Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
